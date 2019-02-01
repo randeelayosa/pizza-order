521b13c82f013cad4ae6ea2811bb6c046783d803
Randee Layosa <randeelayosa@gmail.com>
2019-02-01 09:51:28 -1000

    Add input fields in the html.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function Pizza() {
+  this.order = []
+}
+
+Pizza.prototype.addOrder = function(order) {
+  this.orders.push(order);
+}
+
+function Order(name, size, topping) {
+  this.name = name,
+  this.size = size,
+  this.topping = topping
+}
+  console.log(order)
+
+var pizza = new Pizza();
+
+function displayOrderDetails(pizzaToDisplay) {
+  var orderList = $()
+}
+
+
+
+$(document).ready(function() {
+
+
+
+    function showOrder
+
+
+
+
+
+});
